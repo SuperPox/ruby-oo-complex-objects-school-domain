81ae5a9fd3452ad147dfe6b30e4b87f716c0a84b
SuperPox <calebkohl@gmail.com>
2021-01-21 18:39:32 -0700

    commit tree f640522b18ecea296feb35f7502c8fc34a3605e3

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School
+    
+    attr_accessor :name, :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name,grade) # hint? ||=
+        if @roster.key?(grade) 
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade) # return all of the students in that grade: ["Zach Morris", "AC Slater"] 
+        return @roster[grade]
+    end
+
+    def sort #{9 => ["AC Slater", "Zach Morris"], 10 => ["Aardvark", "Kelly Kapowski"]} 
+        # return @roster.values.sort.name
+        #@roster.values.sort_by { |value| value }.to_h
+        #@roster.values.sort
+        newHash = {}
+        @roster.each {|gradeKey,studentsValue| newHash[gradeKey] = studentsValue.sort}
+        return newHash
+        #@roster.each do value.sort
+        
+        #return @roster
+    end
+end
+
